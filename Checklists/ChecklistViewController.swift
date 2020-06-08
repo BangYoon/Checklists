53e59f7bcf82fdb001ca7b6a954034008b67fdc2
bangyoon <yoonb720@naver.com>
2020-06-08 20:09:32 +0900

    Finish tableView

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -17,13 +17,27 @@ class ChecklistViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 100
     }
     
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Eat ice cream"
+        }
         return cell
     }
 }
