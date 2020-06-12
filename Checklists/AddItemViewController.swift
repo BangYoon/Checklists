a67d3e3148c0c2b3085df7a04541d11e73bdd422
bangyoon <yoonb720@naver.com>
2020-06-12 12:35:09 +0900

    finish staticTable 6

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -10,22 +10,33 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
+    /* Initial Keyboard */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // Mark:- Actions
     @IBAction func cancel() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
 
     /*
